191e524779cd22cd1145701a1a9404bd7a530263
sajibmh-2026 <sajibmh-2026@users.noreply.github.com>
2026-06-28 02:11:01 +0600

    setup: express app with CORS, cookie parser and route mounting

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const ideaRoutes = require("./routes/ideaRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://ideavault.vercel.app"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/ideas", ideaRoutes);
+app.use("/comments", commentRoutes);
+app.use("/users", userRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("IdeaVault Server is Running!");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+app.listen(port, () => {
+  console.log(`IdeaVault Server running on port ${port}`);
+});
